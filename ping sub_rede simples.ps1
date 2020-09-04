dd638263cf060713d2a0af0c6a42993da2064ec5
diasdmhub <67486897+diasdmhub@users.noreply.github.com>
2020-09-04 11:35:38 -0300

    Add files via upload
    Verificação simples de hosts ativos em uma sub-rede utilizando o Test-Connection do PowerShell

---
+++ ping sub_rede simples.ps1
@@ -0,0 +1,42 @@
+﻿<##############################################
+# by diasdm                                   #
+# 2020/01/29 - Versão 1.0                     #
+# 2020/02/04 - Versão 1.1 Uso do Test-connect #
+###############################################
+# Verificação simples de hosts ativos em uma  #
+# sub-rede                                    #
+##############################################>
+
+
+# INFORMAR A FAIXA DE REDE NO FORMATO AAA.BBB.CCC
+cls
+Write-Host "Digite a faixa de sub-rede que deseja verificar no formato (AAA.BBB.CCC)."
+$faixa = Read-Host -Prompt "=>"
+#$faixa = $Host.ui.ReadLine()
+
+
+# VERIFICAÇÃO DOS IPs ATIVOS EM UMA SUB-REDE CLASSE C
+cls
+Write-Host ""
+
+for ($i = 1 ; $i -lt 255 ; $i++ ){
+    if ( Test-Connection -Count 1 -Delay 1 -Quiet -ErrorAction Stop -ComputerName $faixa'.'$i ) {
+    
+        # RESPOSTA POSITIVA EM BRANCO
+        Write-Host -ForegroundColor Black -BackgroundColor White "$faixa`.$i"
+        
+    }
+    
+    Else {
+        
+        # RESPOSTA NEGATIVA EM VERMELHO
+        Write-Host -ForegroundColor White -BackgroundColor Red   "$faixa`.$i"
+        
+    }
+}
+
+
+# LEGENDA
+Write-Host "`n`nLEGENDA"
+Write-Host -ForegroundColor Black -BackgroundColor White "Host ATIVO"
+Write-Host -ForegroundColor White -BackgroundColor Red   "Host NÃO RESPONDE`n"
