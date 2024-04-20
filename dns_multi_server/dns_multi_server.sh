b2ea3d93b91abafb35589d647a3b7324a9625031
diasdmhub <67486897+diasdmhub@users.noreply.github.com>
2024-04-20 14:04:38 -0300

    Updated quotes and read

--- dns_multi_server/dns_multi_server.sh
+++ dns_multi_server/dns_multi_server.sh
@@ -6,26 +6,26 @@
 # EITHER PASS DOMAIN NAME AS ARGUMENT OR ASK FOR IT
 echo -e "\n"
 domain=$1
-[ -z $domain ] && read -p "DOMAIN NAME: " domain
+[ -z "$domain" ] && read -r -p "DOMAIN NAME: " domain
 
 # PATH TO DNS SERVER LIST. IF NOT SET, USE DEFAULT
 dnslist=$2
-[ -z $dnslist ] && read -p "DNS LIST PATH (default = './dnslist'): " dnslist
-[ -z $dnslist ] && dnslist="./dnslist"
+[ -z "$dnslist" ] && read -r -p "DNS LIST PATH (default = './dnslist'): " dnslist
+[ -z "$dnslist" ] && dnslist="./dnslist"
 
 # QUERY FUNCTION USING DIG
 query(){
-        dig +timeout=1 +short "@$1" $2 A 2> /dev/null
+        dig +timeout=1 +short "@$1" "$2" A 2> /dev/null
 }
 
 # MAIN LOOP
 echo -e "\n"
 for dnsserver in $(cat "$dnslist"); do
         result="OK"
-        IPADD=$(query $dnsserver $domain)
+        IPADD=$(query "$dnsserver" "$domain")
 
         # AVOID TIMEOUT BIG RESPONSE
-        echo $IPADD | grep -s -q "timed out" && { IPADD="Timeout" ; result="FAIL" ; }
+        echo "$IPADD" | grep -s -q "timed out" && { IPADD="Timeout" ; result="FAIL" ; }
 
         echo "$result   > Nameserver: $dnsserver - Domain: $domain - IP answer: $IPADD"
 done
